7dfc4d8a582a4d32ea7023e2dcc759dfc83ddb77
Flynn <flynnmarsh98@gmail.com>
2025-03-11 19:38:03 +0100

    First successful simulated device run with Atinary

--- src/robot_controller/tools.py
+++ src/robot_controller/tools.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import random
 import logging
 import sys
 
@@ -7,7 +8,7 @@ from sdlabs_wrapper.wrapper import initialize_optimization
 
 from robot_controller import admiral, hardware_scheduler, pipette_controller
 
-config_file = "data/config/example.json" #conductivity_optimiser.json"
+config_file = "data/config/conductivity_optimiser.json"
 API_KEY = "eyJhbGciOiJIUzUxMiIsImtpZCI6ImtleV9lMmJiY2M4ZWVhMjU0MjU2ODVmZDUzMWE2ZTJmOTE1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2F1dGguYXRpbmFyeS5jb20iLCJjb2duaXRvOmdyb3VwcyI6WyJDQVBlWF9QaW9uZWVyX0NlbnRlciJdLCJpYXQiOjE3NDE3MTE4OTUsIm5iZiI6MTc0MTcxMTg5NSwidXNlcm5hbWUiOiJmMmM2ZDBiYy01OTQ1LTRiM2UtYjA3Mi0yMzc5ZTI1YmI0NjgifQ.caBOaBaSHE-IS-1zgcbGb7jzR05jry_X1i5gArasfSR_k5qy8BDx4tSDz8JTfCXMDMmVtjl4KoNU9LcDykk0HA"
 
 logging.basicConfig(level = logging.INFO)
@@ -17,7 +18,7 @@ def run_campaign() -> None:
     parser.add_argument("--device", help="Used to locate the device data by matching with Device ID.", type=str)
     parser.add_argument("--resume", default=False, help="Continue from last state. Defaults to false to restart.", type=bool, action=argparse.BooleanOptionalAction)
     parser.add_argument("--home", default=False, help="Set true to home gantry on start up. Defaults to false.", type=bool, action=argparse.BooleanOptionalAction)
-    parser.add_argument("--sleep", default=2, help="Sleep time (in minutes) between attempts to get new suggestions from Atinary. Defaults to 2mins.", type=int)
+    parser.add_argument("--sleep", default=30, help="Sleep time (in seconds) between attempts to get new suggestions from Atinary. Defaults to 30s.", type=int)
     parser.add_argument("--temp", default=25, help="Temperature set point for electrolyte analysis. Defaults to 25C.", type=float)
     parser.add_argument("--csv", default="electrolyte_recipe", help="Name of csv file to be updated by Atinary wrapper. Defaults to electrolyte_recipe, or current_state if resume is True.", type=str)
 
@@ -42,15 +43,15 @@ def run_campaign() -> None:
     for iteration in range(wrapper.config.budget):
 
         logging.info(f"Iteration {iteration+1}: Fetching new suggestions..")
-        suggestions = wrapper.get_new_suggestions(max_retries=10, sleep_time_s=args.sleep * 60)
+        suggestions = wrapper.get_new_suggestions(max_retries=10, sleep_time_s=args.sleep)
 
         if not suggestions:
-            logging.error("No suggestions provided, skipping to next iteration.")
-            continue
-
-        logging.info(f"Iteration {iteration+1} New suggestions: {suggestions.param_values}.")
+            logging.error(f"No suggestions received on iteration {iteration+1}.")
+            sys.exit()
 
         for suggestion in suggestions:
+            logging.info(f"Iteration {iteration+1} New suggestion: {suggestion.param_values}.")
+
             # Update df with new volumes and save to current state
             # e.g. {'Zn(ClO4)2': 5.0, 'ZnCl2': 5.0} - names must exactly match those in CSV
             device.update_dose_volumes(suggestion.param_values)
@@ -73,6 +74,36 @@ def run_campaign() -> None:
 
     device.close_all_ports()
     sys.exit()
+
+def test_atinary() -> None:
+    # load config as dict
+    with open(config_file, "rb") as f:
+        config_dict = json.load(f)
+    
+    wrapper = initialize_optimization(
+        api_key=API_KEY,
+        spec_file_content=config_dict,
+        inherit_data=False, 
+        always_restart=True,
+    )
+
+    for iteration in range(wrapper.config.budget):
+        print(f"Iteration {iteration+1}: Fetching new suggestions")
+        suggestions = wrapper.get_new_suggestions(max_retries=10, sleep_time_s=30)
+
+        print(f"Iteration {iteration+1} New Suggestions: {suggestions}")
+
+        for suggestion in suggestions:
+            print(suggestion.param_values)
+
+            for obj in wrapper.config.objectives:
+                suggestion.measurements[obj.name] = random.random()
+
+        print(suggestion.measurements)
+
+        if suggestions:
+            wrapper.send_measurements(suggestions)
+            print(f"Iteration {iteration+1} Measurements sent")
 
 def test_pipette() -> None:
     parser=argparse.ArgumentParser(description="Try out Pipette variables for aspiration and dispense.")
